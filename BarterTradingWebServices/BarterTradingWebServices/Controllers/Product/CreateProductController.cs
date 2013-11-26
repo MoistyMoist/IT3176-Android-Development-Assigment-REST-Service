84c93ed9f21a18efc027f368e7e7653e99979279
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-11-26 18:26:04 +0800

    vcxvcx

--- BarterTradingWebServices/BarterTradingWebServices/Controllers/Product/CreateProductController.cs
+++ BarterTradingWebServices/BarterTradingWebServices/Controllers/Product/CreateProductController.cs
@@ -9,31 +9,6 @@ namespace BarterTradingWebServices.Controllers.Product
 {
     public class CreateProductController : ApiController
     {
-        // GET api/createproduct
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/createproduct/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/createproduct
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/createproduct/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/createproduct/5
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
