04722e9a6f3b0622a244b32f310aecba4a128ba0
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-11-26 18:43:51 +0800

    dsad

---
+++ BarterTradingWebServices/BarterTradingWebServices/Model.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\MoistyBurger\Documents\GitHub\IT3176-Android-Development-Assigment REST Service\BarterTradingWebServices\BarterTradingWebServices\Model.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
