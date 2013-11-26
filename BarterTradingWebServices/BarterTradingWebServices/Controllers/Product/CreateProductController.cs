82c420ebf5af79cb4d93d8ed115117202f35b059
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-11-26 19:14:06 +0800

    method header done left content

--- BarterTradingWebServices/BarterTradingWebServices/Controllers/Product/CreateProductController.cs
+++ BarterTradingWebServices/BarterTradingWebServices/Controllers/Product/CreateProductController.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using BarterTradingWebServices.Model;
 
 namespace BarterTradingWebServices.Controllers.Product
 {
     public class CreateProductController : ApiController
     {
-        
+        [HttpGet]
+        public ProductModel createProduct(string token, string INname, string INdescription, string INqty, string INstatus, string INx, string INy, string INquality, string INimageURL)
+        {
+            return null;           
+        }
     }
 }
--- BarterTradingWebServices/BarterTradingWebServices/Controllers/Product/UpdateProductController.cs
+++ BarterTradingWebServices/BarterTradingWebServices/Controllers/Product/UpdateProductController.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using BarterTradingWebServices.Model;
 
 namespace BarterTradingWebServices.Controllers.Product
 {
     public class UpdateProductController : ApiController
     {
-       
+        [HttpGet]
+        public ProductModel UpdateProduct(string token,string INid, string INname, string INdescription, string INqty, string INstatus, string INx, string INy, string INquality, string INimageURL)
+        {
+            return null;
+        }
     }
 }
