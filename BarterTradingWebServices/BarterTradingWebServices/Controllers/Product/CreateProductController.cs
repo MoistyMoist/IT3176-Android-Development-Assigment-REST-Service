9af5103aa885623c7548982a8ed90251b22cf95a
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-11-26 21:50:15 +0800

    Create product service done

--- BarterTradingWebServices/BarterTradingWebServices/Controllers/Product/CreateProductController.cs
+++ BarterTradingWebServices/BarterTradingWebServices/Controllers/Product/CreateProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data.Entity.Validation;
 using BarterTradingWebServices.Model;
 
 namespace BarterTradingWebServices.Controllers.Product
@@ -15,9 +16,71 @@ namespace BarterTradingWebServices.Controllers.Product
         {
             using (BarterTradingDBEntities db = new BarterTradingDBEntities())
             {
+                int result = 0;
+                List<string> errors = new List<string>();
+                db.Configuration.LazyLoadingEnabled = false;
+                db.Configuration.ProxyCreationEnabled = false;
 
-            }
-            return null;           
+                var query = db.PRODUCTs;
+                List<PRODUCT> OUTProducts = query.ToList();
+
+                PRODUCT newProduct = new PRODUCT();
+                newProduct.name = INname;
+                newProduct.description = INdescription;
+                newProduct.qty = INqty;
+                newProduct.quality = INquality;
+                newProduct.x = INx;
+                newProduct.y = INy;
+                newProduct.imageURL = INimageURL;
+                newProduct.status = 0;
+               
+                if(token.Equals("token"))
+                {
+                    query.Add(newProduct);
+                    try
+                    {
+                        result = db.SaveChanges();
+                    }
+                    catch(DbEntityValidationException dbEx)
+                    {
+                        result = 0;
+                        foreach(var validationErrors in dbEx.EntityValidationErrors)
+                        {
+                            foreach(var validationError in validationErrors.ValidationErrors)
+                            {
+                                string error = "Property: " + validationError.PropertyName + "Error: " + validationError.ErrorMessage;
+                                errors.Add(error);
+                                System.Diagnostics.Debug.WriteLine("Property: {0} Error: {1}",validationError.PropertyName,validationError.ErrorMessage);
+                            }
+                        }
+                    }
+                    if(result==0)
+                    {
+                        ProductModel model = new ProductModel();
+                        model.Status = 1;
+                        model.Message = "Failed to add product";
+                        model.Errors = errors;
+                        model.Data = null;
+                        return model;
+                    }
+                    else
+                    {
+                        ProductModel model = new ProductModel();
+                        model.Status = 0;
+                        model.Message = "Product added successfullt";
+                        model.Errors = errors;
+                        model.Data = new List<PRODUCT> { newProduct };
+                        return model;
+                    }
+                }
+                else
+                {
+                    ProductModel model = new ProductModel();
+                    model.Status = 1;
+                    model.Message = "Token error, invalid token";
+                    return model;
+                }
+            }           
         }
     }
 }
