bf6f2102225a0d044088d38db3e03f3d06b47b44
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-11-26 22:14:35 +0800

    delete service added

--- BarterTradingWebServices/BarterTradingWebServices/Controllers/Product/DeleteControllerController.cs
+++ BarterTradingWebServices/BarterTradingWebServices/Controllers/Product/DeleteControllerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data.Entity.Validation;
 using BarterTradingWebServices.Model;
 
 namespace BarterTradingWebServices.Controllers.Product
@@ -11,13 +12,80 @@ namespace BarterTradingWebServices.Controllers.Product
     public class DeleteControllerController : ApiController
     {
         [HttpGet]
-        public ProductModel removeProduct(string token, string id)
+        public ProductModel removeProduct(string token, string INid)
         {
             using (BarterTradingDBEntities db= new BarterTradingDBEntities())
             {
-                
+                int result = 0;
+                List<string> errors = new List<string>();
+                db.Configuration.LazyLoadingEnabled = false;
+                db.Configuration.ProxyCreationEnabled = false;
+
+                var query = from c in db.PRODUCTs
+                            where (c.productID == Convert.ToInt32(INid))
+                            select c;
+
+                List<PRODUCT> OUTProducts = query.ToList();
+
+                if (token.Equals("token"))
+                {
+                    if (OUTProducts.Count > 0)
+                    {
+                        PRODUCT product = query.ToList().ElementAt(0);
+                        product.status = 2;
+
+                        try
+                        {
+                            result = db.SaveChanges();
+                        }
+                        catch (DbEntityValidationException dbEx)
+                        {
+                            result = 0;
+                            foreach (var validationErrors in dbEx.EntityValidationErrors)
+                            {
+                                foreach (var validationError in validationErrors.ValidationErrors)
+                                {
+                                    string error = "Property: " + validationError.PropertyName + "Error: " + validationError.ErrorMessage;
+                                    errors.Add(error);
+                                    System.Diagnostics.Debug.WriteLine("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                                }
+                            }
+                        }
+                        if (result == 0)
+                        {
+                            ProductModel model = new ProductModel();
+                            model.Status = 1;
+                            model.Message = "Failed to delete product";
+                            model.Errors = errors;
+                            model.Data = null;
+                            return model;
+                        }
+                        else
+                        {
+                            ProductModel model = new ProductModel();
+                            model.Status = 0;
+                            model.Message = "Product deleted successfullt";
+                            model.Errors = errors;
+                            model.Data = new List<PRODUCT> { product };
+                            return model;
+                        }
+                    }
+                    else
+                    {
+                        ProductModel model = new ProductModel();
+                        model.Status = 1;
+                        model.Message = "Product does not exist";
+                        return model;
+                    }
+                }
+                else
+                {
+                    ProductModel model = new ProductModel();
+                    model.Status = 1;
+                    model.Message = "Token error, invalid token";
+                    return model;
+                }
             }
-            return null;
         }
     }
 }
