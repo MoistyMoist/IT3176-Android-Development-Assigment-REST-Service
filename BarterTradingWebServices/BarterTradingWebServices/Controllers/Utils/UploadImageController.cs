e61dce812ddd259d9b1467aef53c4194cc8d86c6
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-12-02 22:37:57 +0800

    working on the upload image

--- BarterTradingWebServices/BarterTradingWebServices/Controllers/Utils/UploadImageController.cs
+++ BarterTradingWebServices/BarterTradingWebServices/Controllers/Utils/UploadImageController.cs
@@ -13,7 +13,7 @@ namespace BarterTradingWebServices.Controllers.Utils
     public class UploadImageController : ApiController
     {
         [HttpGet]
-        public string UploadImage(string token)
+        public string UploadImage(string token, StreamContent file, string INfileName)
         {
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Microsoft.WindowsAzure.CloudConfigurationManager.GetSetting("StorageConnectionString"));
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
@@ -23,8 +23,14 @@ namespace BarterTradingWebServices.Controllers.Utils
             // Retrieve a reference to a container. 
             CloudBlobContainer container = blobClient.GetContainerReference("bartertrading");
             container.CreateIfNotExists();
-            // Create the container if it doesn't already exist.
-          
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(INfileName);
+
+            // Create or overwrite the "myblob" blob with contents from a local file.
+            using (var fileStream = System.IO.File.OpenRead(@"path\myfile"))
+            {
+               // blockBlob.UploadFromStream(fileStream);
+            }
 
             return "dsad";
         }
