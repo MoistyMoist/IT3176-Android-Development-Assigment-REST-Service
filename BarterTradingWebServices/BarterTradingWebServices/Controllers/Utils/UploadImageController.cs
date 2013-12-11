6e5e9028e5249edab41a85b2a193035acc0d4284
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-12-11 12:37:10 +0800

    testing image upload

--- BarterTradingWebServices/BarterTradingWebServices/Controllers/Utils/UploadImageController.cs
+++ BarterTradingWebServices/BarterTradingWebServices/Controllers/Utils/UploadImageController.cs
@@ -8,13 +8,35 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System.Configuration;
+using System.IO;
+using System.Drawing;
+
+
 namespace BarterTradingWebServices.Controllers.Utils
 {
     public class UploadImageController : ApiController
     {
         [HttpGet]
-        public string UploadImage(string token, StreamContent file, string INfileName)
+        public string UploadImage(string token, string base64, string INfileName)
         {
+            byte[] imageBytes = Convert.FromBase64String(base64);
+            MemoryStream ms = new MemoryStream(imageBytes, 0,imageBytes.Length);
+
+            // Convert byte[] to Image
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            Image image = Image.FromStream(ms, true);
+
+
+
+
+            image.Save("c:\\myBitmap.jpg");
+
+
+
+
+
+            /*
+
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Microsoft.WindowsAzure.CloudConfigurationManager.GetSetting("StorageConnectionString"));
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
 
@@ -25,12 +47,13 @@ namespace BarterTradingWebServices.Controllers.Utils
             container.CreateIfNotExists();
 
             CloudBlockBlob blockBlob = container.GetBlockBlobReference(INfileName);
-
             // Create or overwrite the "myblob" blob with contents from a local file.
+            
             using (var fileStream = System.IO.File.OpenRead(@"path\myfile"))
             {
-               // blockBlob.UploadFromStream(fileStream);
+                blockBlob.UploadFromFile(image, ".JPG");
             }
+             * */
 
             return "dsad";
         }
