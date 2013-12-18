61ba20aafba251fcd7636f38f8dc565ca55e16cb
eddietcs <eddietcs@hotmail.com>
2013-12-18 18:35:47 +0800

    retrieve user

--- BarterTradingWebServices/BarterTradingWebServices/Controllers/User/RetrieveUserController.cs
+++ BarterTradingWebServices/BarterTradingWebServices/Controllers/User/RetrieveUserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BarterTradingWebServices.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,6 +10,43 @@ namespace BarterTradingWebServices.Controllers.User
 {
     public class RetrieveUserController : ApiController
     {
-        
+        [HttpGet]
+        public UserModel GetProductByID(string token, string email)
+        {
+            using (BarterTradingDBEntities db = new BarterTradingDBEntities())
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+
+                var query = from c in db.USERs
+                            where (c.email.Equals(email))
+                            select c;
+                List<USER> OUTUser = query.ToList();
+                if (token.Equals("token"))
+                {
+                    if (OUTUser.Count() == 0)
+                    {
+                        UserModel model = new UserModel();
+                        model.Status = 1;
+                        model.Message = "Error retrieveing products";
+                        return model;
+                    }
+                    else
+                    {
+                        UserModel model = new UserModel();
+                        model.Status = 0;
+                        model.Message = "Retrieve success";
+                        model.Data = OUTUser;
+                        return model;
+                    }
+                }
+                else
+                {
+                    UserModel model = new UserModel();
+                    model.Status = 1;
+                    model.Message = "Token error, invalid token";
+                    return model;
+                }
+            }
+        }
     }
 }
