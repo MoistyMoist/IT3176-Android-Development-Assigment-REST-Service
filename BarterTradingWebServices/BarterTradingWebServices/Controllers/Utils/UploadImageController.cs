4155534f1ccb9585ca64606bc3756c940a6832c0
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-12-18 18:00:11 +0800

    uploadimage function completed!!

--- BarterTradingWebServices/BarterTradingWebServices/Controllers/Utils/UploadImageController.cs
+++ BarterTradingWebServices/BarterTradingWebServices/Controllers/Utils/UploadImageController.cs
@@ -10,52 +10,45 @@ using Microsoft.WindowsAzure.Storage.Blob;
 using System.Configuration;
 using System.IO;
 using System.Drawing;
-
+using BarterTradingWebServices.Model;
 
 namespace BarterTradingWebServices.Controllers.Utils
 {
     public class UploadImageController : ApiController
     {
-        [HttpGet]
-        public string UploadImage(string token, string base64, string INfileName)
+        [HttpPost]
+        public ImageModel postTest([FromBody] string base64)
         {
-            byte[] imageBytes = Convert.FromBase64String(base64);
-            MemoryStream ms = new MemoryStream(imageBytes, 0,imageBytes.Length);
-
-            // Convert byte[] to Image
-            ms.Write(imageBytes, 0, imageBytes.Length);
-            Image image = Image.FromStream(ms, true);
-
-
-
-
-            image.Save("c:\\myBitmap.jpg");
-
-
-
-
-
-            /*
+            DateTime date = new DateTime();
+            string fileName = date.ToString();
 
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Microsoft.WindowsAzure.CloudConfigurationManager.GetSetting("StorageConnectionString"));
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
 
             storageAccount.CreateCloudBlobClient();
             // Retrieve a reference to a container. 
             CloudBlobContainer container = blobClient.GetContainerReference("bartertrading");
             container.CreateIfNotExists();
 
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference(INfileName);
-            // Create or overwrite the "myblob" blob with contents from a local file.
-            
-            using (var fileStream = System.IO.File.OpenRead(@"path\myfile"))
-            {
-                blockBlob.UploadFromFile(image, ".JPG");
-            }
-             * */
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
 
-            return "dsad";
+            byte[] bytes = Convert.FromBase64String(base64);
+
+            using (var stream = new MemoryStream(bytes))
+            {
+                blockBlob.UploadFromStream(stream);
+            }
+
+            string url = "http://114173s.blob.core.windows.net/bartertrading/" + fileName + ".jpg";
+
+            ImageModel model = new ImageModel();
+            model.Data = url;
+            model.Message = base64.Length.ToString();
+            model.Status = 1;
+            return model;
+
+
+
         }
     }
 }
---
+++ BarterTradingWebServices/BarterTradingWebServices/Model/ImageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BarterTradingWebServices.Model
+{
+    public class ImageModel
+    {
+        public string Message { get; set; }
+        public int Status { get; set; }
+        public string Data { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
